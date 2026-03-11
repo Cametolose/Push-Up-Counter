cceb7435f241ffe188d1cf54575d155946502c80
Cametolose <102823827+Cametolose@users.noreply.github.com>
2026-03-11 17:55:46 +0100

    Removed useless dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.firebase:firebase-auth")
-    implementation("com.google.firebase:firebase-database")
     implementation("com.google.android.gms:play-services-auth:21.1.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
